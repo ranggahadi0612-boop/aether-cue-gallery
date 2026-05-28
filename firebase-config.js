ccff3fc220d09a1140dbe21a26418e5379c690c4
ranggahadi0612-boop <ranggahadi0612@gmail.com>
2026-05-28 09:07:19 +0700

    fix firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,8 @@
 const firebaseConfig = {
-    apiKey: "ISI_API_KEY_KAMU",
-    authDomain: "ISI_AUTH_DOMAIN_KAMU",
-    projectId: "ISI_PROJECT_ID_KAMU",
-    storageBucket: "ISI_STORAGE_BUCKET_KAMU",
-    messagingSenderId: "ISI_MESSAGING_ID_KAMU",
-    appId: "ISI_APP_ID_KAMU"
+  apiKey: "AIzaSyBmr6s_NwwjP3rwhoWFBYeOF-jRz6KtYmY",
+  authDomain: "aether-cue-gallery.firebaseapp.com",
+  projectId: "aether-cue-gallery",
+  storageBucket: "aether-cue-gallery.firebasestorage.app",
+  messagingSenderId: "957870196198",
+  appId: "1:957870196198:web:6b3b171a4b304d9c3cc227"
 };
-
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
